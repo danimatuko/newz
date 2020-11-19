e47879fb538c86c43c722a049216ff7d3aee34ef
Dani Matuko <matuko305@gmail.com>
2020-11-19 20:36:42 +0200

    heroku proxy1

--- server/server.js
+++ server/server.js
@@ -2,12 +2,22 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const routes = require("./routes/routes");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
+const path = require("path");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/", routes);
 
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets
+  app.use(express.static("../client/build"));
+
+  // Express will serve up the front-end index.html file if it doesn't recognize the route
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
