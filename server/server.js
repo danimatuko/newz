3b8dbbb30309d32e5fbc01063c75559be118c4d3
Dani Matuko <matuko305@gmail.com>
2020-11-19 20:25:32 +0200

    heroku proxy1

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const port = 5000;
 require("dotenv").config();
 const routes = require("./routes/routes");
 const bodyParser = require('body-parser');
@@ -8,6 +7,8 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/", routes);
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Listening to NEWZ server at http://localhost:${port}`);
